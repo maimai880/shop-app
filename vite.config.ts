cb97adbb1a5891cbff7ccbb4646fd21508e2ca82
ao-takeda <ao_21n1103123@nnn.ed.jp>
2022-11-19 18:31:55 +0900

    @のimportが上手くいかないため、rootをsrcからプロジェクトトップに戻した

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,6 @@ export default defineConfig({
   server: {
     open: true,
   },
-  root: './src',
-  publicDir: '../public',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
