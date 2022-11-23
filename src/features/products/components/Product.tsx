51c94ddfa5a6c360c2f879db9f2ac6c1fa59ecdc
ao-takeda <ao_21n1103123@nnn.ed.jp>
2022-11-23 18:13:37 +0900

    商品画像モーダル実装

--- src/features/products/components/Product.tsx
+++ src/features/products/components/Product.tsx
@@ -4,7 +4,13 @@ import {
   Flex,
   HStack,
   Input,
+  Modal,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalOverlay,
   Text,
+  useDisclosure,
   useNumberInput
 } from '@chakra-ui/react'
 import { ProductType } from '@/features/products/types'
@@ -26,37 +32,79 @@ export const Product: FC<Props> = ({ data }) => {
     min: 0
   })
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
-    <Flex
-      bg="white"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      overflow="hidden"
-    >
-      <img src={data.image} alt={data.name} className={styles.image} />
+    <>
+      <Flex
+        bg="white"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        overflow="hidden"
+      >
+        <img
+          src={data.image}
+          alt={data.name}
+          className={styles.image}
+          onClick={onOpen}
+        />
 
-      <Text fontSize="md" color="#666" align="center">
-        {data.name}
-      </Text>
+        <Text fontSize="md" color="#666" align="center">
+          {data.name}
+        </Text>
 
-      <Text fontSize="lg" as="b" color="#666" align="center" marginBottom="5px">
-        ￥{data.price}
-      </Text>
+        <Text
+          fontSize="lg"
+          as="b"
+          color="#666"
+          align="center"
+          marginBottom="5px"
+        >
+          ￥{data.price}
+        </Text>
 
-      <HStack maxW="80%">
-        <Button {...getDecrementButtonProps()} rounded="full" size="sm">
-          -
+        <HStack maxW="80%">
+          <Button {...getDecrementButtonProps()} rounded="full" size="sm">
+            -
+          </Button>
+          <Input {...getInputProps()} size="sm" />
+          <Button {...getIncrementButtonProps()} rounded="full" size="sm">
+            +
+          </Button>
+        </HStack>
+
+        <Button margin="15px" bg="#077915" color="white">
+          ADD TO CART
         </Button>
-        <Input {...getInputProps()} size="sm" />
-        <Button {...getIncrementButtonProps()} rounded="full" size="sm">
-          +
-        </Button>
-      </HStack>
+      </Flex>
 
-      <Button margin="15px" bg="#077915" color="white">
-        ADD TO CART
-      </Button>
-    </Flex>
+      <ImageModal product={data} {...{ isOpen, onClose }} />
+    </>
+  )
+}
+
+interface ImageModalProps {
+  product: ProductType
+  isOpen: boolean
+  onClose: () => void
+}
+
+const ImageModal: FC<ImageModalProps> = (props) => {
+  return (
+    <Modal isOpen={props.isOpen} onClose={props.onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton />
+        <img src={props.product.image} />
+
+        <ModalFooter bg="#f1f1f1" justifyContent="space-between">
+          <Text>{props.product.name}</Text>
+          <Text color="#077915" as="b">
+            ￥{props.product.price}
+          </Text>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   )
 }
