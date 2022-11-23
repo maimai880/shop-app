45e7a7908d51547a09a6e4bbd6112f69e53f5650
ao-takeda <ao_21n1103123@nnn.ed.jp>
2022-11-23 17:27:45 +0900

    商品の型定義

--- src/features/products/api/getProducts.ts
+++ src/features/products/api/getProducts.ts
@@ -1,8 +1,13 @@
 import { axios } from '@/lib/axios'
 import { useQuery } from '@tanstack/react-query'
 import { ExtractFnReturnType, QueryConfig } from '@/lib/react-query'
+import { ProductType } from '../types'
 
-export const getProducts = ({ query }: { query?: string }) => {
+export const getProducts = ({
+  query
+}: {
+  query?: string
+}): Promise<ProductType[]> => {
   return axios.get('/products')
 }
 
