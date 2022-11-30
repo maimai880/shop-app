93fbd002775a1bc51fdc49ab623ce7f5e24c19b0
ao-takeda <ao_21n1103123@nnn.ed.jp>
2022-11-30 16:01:32 +0900

    ログインページの見た目修正

--- src/features/auth/component/LoginPage.tsx
+++ src/features/auth/component/LoginPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Box, Center, Heading } from '@chakra-ui/react'
+import { Box, Center } from '@chakra-ui/react'
 import { LoginForm } from '@/features/auth/component/LoginForm'
+import { Logo } from '@/component/logo'
 
 interface Props {}
 
@@ -8,18 +9,16 @@ export const LoginPage: React.FC<Props> = (props) => {
   return (
     <Center width="100%" height="100%">
       <Box bg="white" p={4} position="relative">
-        <Heading
+        <Logo
           position="absolute"
           top="0"
           left="50%"
-          transform="translate(-50%, -105%)"
+          transform="translate(-50%, -110%)"
           textAlign="center"
+          fontSize={42}
+          lineHeight={10}
           color="#077915"
-        >
-          SHOP
-          <br />
-          APP
-        </Heading>
+        />
 
         <LoginForm />
       </Box>
