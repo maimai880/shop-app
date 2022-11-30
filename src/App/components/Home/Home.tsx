3216bb3f22a49fede1c29e1ef377cb2ba0e2092e
ao-takeda <ao_21n1103123@nnn.ed.jp>
2022-11-30 09:53:40 +0900

    検索バーの開閉実装

--- src/App/components/Home/Home.tsx
+++ src/App/components/Home/Home.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import { Box, Center, Flex, Heading, VStack } from '@chakra-ui/react'
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  IconButton,
+  useDisclosure,
+  VStack
+} from '@chakra-ui/react'
 import { ProductsList } from '@/features/products/components/ProductsList'
 import { AuthMenu } from '@/features/auth/component/authMenu'
 import { CartInformation } from '@/features/cart/components/cartInformation'
 import { CartPreview } from '@/features/cart/components/CartPreview'
 import { ProductSearchBar } from '@/features/products/components/ProductSearchBar'
+import { ArrowBackIcon, SearchIcon } from '@chakra-ui/icons'
 
 interface Props {}
 
 export const Home: React.FC<Props> = () => {
+  const { isOpen: isOpenSearchHeader, onOpen, onClose } = useDisclosure()
+
   return (
     <VStack>
       <Center
@@ -23,35 +34,67 @@ export const Home: React.FC<Props> = () => {
         zIndex={3}
       >
         <Flex as="nav" align="center" w="900px" h="100%">
-          <Box mr={{ base: 'auto', sm: '4%' }}>
-            <Heading
-              textAlign="center"
-              fontSize={30}
-              lineHeight={8}
-              color="green"
-            >
-              SHOP
-              <br />
-              APP
-            </Heading>
-          </Box>
+          {isOpenSearchHeader ? (
+            <>
+              <IconButton
+                onClick={onClose}
+                aria-label="close search bar"
+                icon={<ArrowBackIcon h="100%" w="auto" color="green" />}
+                bg="transparent"
+                mr={3}
+              />
+              <ProductSearchBar />
+            </>
+          ) : (
+            <>
+              <Box mr={{ base: 'auto', sm: '4%' }}>
+                <Heading
+                  textAlign="center"
+                  fontSize={30}
+                  lineHeight={8}
+                  color="green"
+                >
+                  SHOP
+                  <br />
+                  APP
+                </Heading>
+              </Box>
 
-          <ProductSearchBar />
+              <Box display={['none', 'inline']} maxW="500px" w="100%" mr={3}>
+                <ProductSearchBar />
+              </Box>
+              <Box display={['inline', 'none']} h="100%">
+                <IconButton
+                  onClick={onOpen}
+                  aria-label="open search bar"
+                  icon={
+                    <SearchIcon
+                      display={['inline', 'none']}
+                      h="100%"
+                      w="auto"
+                      color="green"
+                    />
+                  }
+                  h="35px"
+                  bg="transparent"
+                />
+              </Box>
 
-          <Box
-            display={{ base: 'none', lg: 'inline' }}
-            ml="auto"
-            overflow="visible"
-          >
-            <CartInformation />
-          </Box>
-          <Center ml={{ base: 3, sm: 'auto', md: 'auto', lg: 3 }}>
-            <CartPreview />
-          </Center>
-
-          <Center ml={2}>
-            <AuthMenu />
-          </Center>
+              <Box
+                display={{ base: 'none', lg: 'inline' }}
+                ml="auto"
+                overflow="visible"
+              >
+                <CartInformation />
+              </Box>
+              <Center ml={{ base: 3, sm: 'auto', md: 'auto', lg: 3 }}>
+                <CartPreview />
+              </Center>
+              <Center ml={2}>
+                <AuthMenu />
+              </Center>
+            </>
+          )}
         </Flex>
       </Center>
 
--- src/features/products/components/ProductSearchBar.tsx
+++ src/features/products/components/ProductSearchBar.tsx
@@ -1,4 +1,4 @@
-import { IconButton, Input } from '@chakra-ui/react'
+import { Flex, IconButton, Input } from '@chakra-ui/react'
 import { SearchIcon } from '@chakra-ui/icons'
 import { ChangeEvent } from 'react'
 import { useRecoilValue } from 'recoil'
@@ -14,33 +14,23 @@ export const ProductSearchBar = () => {
   }
 
   return (
-    <>
+    <Flex w="100%">
       <Input
         value={value}
         onChange={handleChange}
-        display={['none', 'inline']}
         placeholder="Search for Vegetables and Fruits"
-        w="42%"
+        maxW="410px"
         border="1px solid green !important"
         rounded={0}
       />
       <IconButton
         icon={<SearchIcon color="white" />}
         aria-label="search products"
-        display={['none', 'inline']}
         w="90px"
         ml="0px !important"
-        mr={3}
         colorScheme="green"
         rounded={0}
       />
-
-      <SearchIcon
-        display={['inline', 'none']}
-        h="100%"
-        w="auto"
-        color="green"
-      />
-    </>
+    </Flex>
   )
 }
